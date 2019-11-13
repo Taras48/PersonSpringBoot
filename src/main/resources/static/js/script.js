69ca2aa4478b82b2d6b49b117a763b2f89cf0f65
Taras48 <red.shuhov48@gmail.com>
2019-11-13 21:47:12 +0300

    готово почти все

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,26 +1,43 @@
 $(document).ready(function () {
 
-    // alert("test 1 ");
+    getTable();
 
-    $.ajax({
-        type: 'GET',
-        url: "/admin/all",
+    //from table
+    function getTable() {
+        $.ajax({
+            type: 'GET',
+            url: "/admin/all",
 
-        contentType: 'application/json;',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        async: true,
-        cache: false,
-        dataType: 'JSON',
-        success: function (listUsers) {
-           alert(JSON.stringify(listUsers));
-        }
+            contentType: 'application/json;',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            async: true,
+            cache: false,
+            dataType: 'JSON',
+            success: function (listUsers) {
+                var htmlTable = "";
+                for (var i = 0; i < listUsers.length; i++) {
+                    htmlTable += ('<tr id="list">');
+                    htmlTable += ('<td id="tableId">' + listUsers[i].id + '</td>');
+                    htmlTable += ('<td id="tableName">' + listUsers[i].name + '</td>');
+                    htmlTable += ('<td id="tableMess>' + listUsers[i].message + '</td>');
+                    htmlTable += ('<td id="tablePass>' + listUsers[i].password + '</td>');
+                    htmlTable += ('<td id="tableRole">' + listUsers[i].roles[0].role + '</td>');
+                    htmlTable += ('<td><button class="btn btn-sm btn-info" type="button" data-toggle="modal" data-target="#editUser">Edit</button></td>');
+                    htmlTable += ('</tr>');
+                }
+                $("#getUserTable thead").after(htmlTable);
+            }
 
-    });
+        });
+    };
 
     $("tr td .btn").click(function () {
+
+        alert("ertyui");
+
         $("#updateUserId").val($(this).closest("tr").find("#tableId").text());
         $("#updateUserId").prop("disabled", true);
 
@@ -44,7 +61,7 @@ $(document).ready(function () {
     $("#addFormUser").click(function (event) {
         event.preventDefault();
         addForm();
-        $("#addForm").refresh();
+        $("#addForm").reset();
     });
 
     function addForm() {
